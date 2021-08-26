b93a65d62e46803a69e4e2bd3be07a6a23896ac7
N_Hashimoto_T <n-hashimoto@tanseisha.co.jp>
2021-08-26 16:12:59 +0900

    名前をクリックするシーンを追加し、今までのシーンと違うシーンを作成

--- docs/Build/docs.framework.js
+++ docs/Build/docs.framework.js
@@ -1308,7 +1308,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 3169104;
+STATICTOP = STATIC_BASE + 3187056;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3358,7 +3358,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 3169104;
+var STATIC_BUMP = 3187056;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
@@ -16138,8 +16138,8 @@ function nullFunc_vjji(x) {
  err("Build with ASSERTIONS=2 for more info.");
  abort(x);
 }
-Module["wasmTableSize"] = 97260;
-Module["wasmMaxTableSize"] = 97260;
+Module["wasmTableSize"] = 105452;
+Module["wasmMaxTableSize"] = 105452;
 function invoke_dddi(index, a1, a2, a3) {
  var sp = stackSave();
  try {
